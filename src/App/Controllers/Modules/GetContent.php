3474d99b9f5f293cfafba03c3dadcc073cd54bbd
Alejandro Tapia <alexchirolde@gmail.com>
2022-12-15 10:34:11 -0500

    Function getFolderContent: get mimetype using https://packagist.org/packages/league/mime-type-detection

--- src/App/Controllers/Modules/GetContent.php
+++ src/App/Controllers/Modules/GetContent.php
@@ -107,7 +107,7 @@ trait GetContent
                 'timestamp' => $attributes->lastModified(),
                 'size' => $attributes->isFile() ? $attributes->fileSize() : 0,
                 'type' => $attributes->isFile() ? 'file' : 'dir',
-                'mimetype' => $attributes->isFile() ? ($attributes->mimeType() ?: $this->getMimeFromExtension($extension)) : 'folder',
+                'mimetype' => $attributes->isFile() ? ($this->storageDisk->mimeType($path)) : 'folder',
                 'etag' => $attributes->extraMetadata()['ETag'] ?? null,
                 'visibility' => $attributes->visibility()
             ];
@@ -151,68 +151,4 @@ trait GetContent
         ];
     }
 
-    protected function getMimeFromExtension($ext) {
-
-        $mime_types = array(
-
-            'txt' => 'text/plain',
-            'htm' => 'text/html',
-            'html' => 'text/html',
-            'php' => 'text/html',
-            'css' => 'text/css',
-            'js' => 'application/javascript',
-            'json' => 'application/json',
-            'xml' => 'application/xml',
-            'swf' => 'application/x-shockwave-flash',
-            'flv' => 'video/x-flv',
-
-            // images
-            'png' => 'image/png',
-            'jpe' => 'image/jpeg',
-            'jpeg' => 'image/jpeg',
-            'jpg' => 'image/jpeg',
-            'gif' => 'image/gif',
-            'bmp' => 'image/bmp',
-            'ico' => 'image/vnd.microsoft.icon',
-            'tiff' => 'image/tiff',
-            'tif' => 'image/tiff',
-            'svg' => 'image/svg+xml',
-            'svgz' => 'image/svg+xml',
-
-            // archives
-            'zip' => 'application/zip',
-            'rar' => 'application/x-rar-compressed',
-            'exe' => 'application/x-msdownload',
-            'msi' => 'application/x-msdownload',
-            'cab' => 'application/vnd.ms-cab-compressed',
-
-            // audio/video
-            'mp3' => 'audio/mpeg',
-            'qt' => 'video/quicktime',
-            'mov' => 'video/quicktime',
-
-            // adobe
-            'pdf' => 'application/pdf',
-            'psd' => 'image/vnd.adobe.photoshop',
-            'ai' => 'application/postscript',
-            'eps' => 'application/postscript',
-            'ps' => 'application/postscript',
-
-            // ms office
-            'doc' => 'application/msword',
-            'rtf' => 'application/rtf',
-            'xls' => 'application/vnd.ms-excel',
-            'ppt' => 'application/vnd.ms-powerpoint',
-
-            // open office
-            'odt' => 'application/vnd.oasis.opendocument.text',
-            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
-        );
-
-        if (array_key_exists($ext, $mime_types)) {
-            return $mime_types[$ext];
-        } else {
-            return 'application/octet-stream';
-        }
-    }
 }
